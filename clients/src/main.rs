6cb28b393aab3d08e6fcb6e87c1d48b235be761d
mthiaw <massaw6@gmail.com>
2024-08-14 11:50:16 +0000

    preversion

--- clients/src/main.rs
+++ clients/src/main.rs
@@ -1,93 +1,135 @@
 use bevy::prelude::*;
-use bevy_rapier3d::plugin::{NoUserData, RapierPhysicsPlugin};
-use crate::player::player::Player;
+//use bevy_rapier3d::plugin::{NoUserData, RapierPhysicsPlugin};
+//use crate::player::player::Player;
 use bevy::sprite::collide_aabb::collide;
+use std::net::{ UdpSocket, SocketAddr };
+use std::time::SystemTime;
+
+//use bevy_renet::transport::NetcodeServerPlugin;
+use bevy_renet::{ renet::*, transport::NetcodeClientPlugin, RenetClientPlugin };
+use bevy::prelude::{ App, ResMut };
+//use bevy::app::*;
+use transport::{ ClientAuthentication, NetcodeClientTransport };
 // use bevy::render::debug::DebugLines;
 
-// use bevy_gltf::Gltf;
-mod  playing_field;
-mod player;
-#[derive(Component)]
-struct GltfWall;
+// // use bevy_gltf::Gltf;
+// mod  playing_field;
+// mod player;
+// #[derive(Component)]
+// struct GltfWall;
 
-
-#[derive(Component)]
-struct MinimapPlayer;
+// #[derive(Component)]
+// struct MinimapPlayer;
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "IBG".into(),
-                resolution: (1500.0, 1000.0).into(),
-                resizable: false,
-                ..default()
-            }),
-            ..default()
-        }))
-        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
-        .add_systems(Startup, (
-            player::player::setup_player_and_camera,
-            playing_field::playing_field::Fields::spawn_ground,
-            // playing_field::playing_field::Fields::spawn_object,
-            // playing_field::playing_field::Fields::spawn_player,
-            setup,
-        ))
-        // .add_systems(Startup, setup)
-        .add_systems(Update,(
-            player::player::move_player, player::player::grab_mouse,
-            // playing_field::playing_field::handle_collisions,
-            handle_gltf_wall_collisions,
-            // debug_draw_system,
-        ).chain())
-        .run();
-}
+    let mut app = App::new();
+    app.add_plugins(RenetClientPlugin);
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    // Charger le modèle
-    let scene_handle: Handle<Scene> = asset_server.load("mages/mage1_2.glb#Scene0");
+    let client = RenetClient::new(ConnectionConfig::default());
+    app.insert_resource(client);
 
-    // Spawner le modèle
-    commands.spawn((
-        SceneBundle {
-            scene: scene_handle,
-            transform: Transform::from_xyz(-5.0, -2.3, -5.0).with_scale(Vec3::splat(0.8)),
-            ..default()
-        },
-        GltfWall,
-    ));
+    // Setup the transport layer
+    app.add_plugins(NetcodeClientPlugin);
+    //let socket = UdpSocket::bind("127.0.0.1:5500").unwrap();
+    let authentication = ClientAuthentication::Unsecure {
+        server_addr: "127.0.0.1:5500".parse().unwrap(),
+        client_id: 0,
+        user_data: None,
+        protocol_id: 0,
+    };
+    let socket = UdpSocket::bind("127.0.0.1:0").unwrap();
+    let current_time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
+    let mut transport = NetcodeClientTransport::new(current_time, authentication, socket).unwrap();
 
-    // Caméra
-    // commands.spawn(Camera3dBundle {
-    //     transform: Transform::from_xyz(0.0, 5.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
-    //     ..default()
-    // });
+    app.insert_resource(transport);
 
-    // Lumière
-    commands.spawn(PointLightBundle {
-        point_light: PointLight {
-            intensity: 1500.0,
-            shadows_enabled: true,
-            ..default()
-        },
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
-        ..default()
-    });
+    app.add_systems(Update, send_message_system);
+    app.add_systems(Update, receive_message_system);
 
-    // Joueur sur la minimap
-    commands.spawn((
-        MinimapPlayer,
-        SpriteBundle {
-            sprite: Sprite {
-                color: Color::RED,
-                ..default()
-            },
-            transform: Transform::from_scale(Vec3::splat(5.0)), // Échelle du joueur sur la minimap
-            ..default()
+    // Systems
+
+    fn send_message_system(mut client: ResMut<RenetClient>) {
+        // Send a text message to the server
+        client.send_message(DefaultChannel::ReliableOrdered, "server message");
+    }
+
+    fn receive_message_system(mut client: ResMut<RenetClient>) {
+        while let Some(message) = client.receive_message(DefaultChannel::ReliableOrdered) {
+            // Handle received message
         }
-    ));
+    }
+    app.run()
+    //     App::new()
+    //         .add_plugins(DefaultPlugins.set(WindowPlugin {
+    //             primary_window: Some(Window {
+    //                 title: "IBG".into(),
+    //                 resolution: (1500.0, 1000.0).into(),
+    //                 resizable: false,
+    //                 ..default()
+    //             }),
+    //             ..default()
+    //         }))
+    //         .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+    //         .add_systems(Startup, (
+    //             player::player::setup_player_and_camera,
+    //             playing_field::playing_field::Fields::spawn_ground,
+    //             // playing_field::playing_field::Fields::spawn_object,
+    //             // playing_field::playing_field::Fields::spawn_player,
+    //             setup,
+    //         ))
+    //         // .add_systems(Startup, setup)
+    //         .add_systems(Update,(
+    //             player::player::move_player, player::player::grab_mouse,
+    //             // playing_field::playing_field::handle_collisions,
+    //             handle_gltf_wall_collisions,
+    //             // debug_draw_system,
+    //         ).chain())
+    //         .run();
+    // }
 
+    // fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    //     // Charger le modèle
+    //     let scene_handle: Handle<Scene> = asset_server.load("mages/mage1_2.glb#Scene0");
 
+    //     // Spawner le modèle
+    //     commands.spawn((
+    //         SceneBundle {
+    //             scene: scene_handle,
+    //             transform: Transform::from_xyz(-5.0, -2.3, -5.0).with_scale(Vec3::splat(0.8)),
+    //             ..default()
+    //         },
+    //         GltfWall,
+    //     ));
+
+    //     // Caméra
+    //     // commands.spawn(Camera3dBundle {
+    //     //     transform: Transform::from_xyz(0.0, 5.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+    //     //     ..default()
+    //     // });
+
+    //     // Lumière
+    //     commands.spawn(PointLightBundle {
+    //         point_light: PointLight {
+    //             intensity: 1500.0,
+    //             shadows_enabled: true,
+    //             ..default()
+    //         },
+    //         transform: Transform::from_xyz(4.0, 8.0, 4.0),
+    //         ..default()
+    //     });
+
+    //     // Joueur sur la minimap
+    //     commands.spawn((
+    //         MinimapPlayer,
+    //         SpriteBundle {
+    //             sprite: Sprite {
+    //                 color: Color::RED,
+    //                 ..default()
+    //             },
+    //             transform: Transform::from_scale(Vec3::splat(5.0)), // Échelle du joueur sur la minimap
+    //             ..default()
+    //         }
+    //     ));
 }
 
 fn check_model_loaded(asset_server: Res<AssetServer>, scene_assets: Res<Assets<Scene>>) {
@@ -99,72 +141,78 @@ fn check_model_loaded(asset_server: Res<AssetServer>, scene_assets: Res<Assets<S
     }
 }
 
+// fn handle_gltf_wall_collisions(
+//     mut player_query: Query<(&mut Transform, &Player)>,
+//     wall_query: Query<&GlobalTransform, With<GltfWall>>
+// ) {
+//     if let Ok((mut player_transform, player)) = player_query.get_single_mut() {
+//         if let Ok((mut player_transform, player)) = player_query.get_single_mut() {
+//             for wall_transform in wall_query.iter() {
+//                 let wall_scale = wall_transform.compute_transform().scale;
+//                 let wall_size = Vec3::new(2.0, 2.0, 2.0) * wall_scale.x;
+//                 let wall_pos = wall_transform.translation();
 
+//                 let collision = collide(
+//                     player_transform.translation,
+//                     Vec2::new(player.size.x, player.size.y),
+//                     wall_pos,
+//                     Vec2::new(wall_size.x, wall_size.z)
+//                 );
 
-fn handle_gltf_wall_collisions(
-    mut player_query: Query<(&mut Transform, &Player)>,
-    wall_query: Query<&GlobalTransform, With<GltfWall>>,
-) {
-    if let Ok((mut player_transform, player)) = player_query.get_single_mut() {
-        if let Ok((mut player_transform, player)) = player_query.get_single_mut() {
-            for wall_transform in wall_query.iter() {
-                let wall_scale = wall_transform.compute_transform().scale;
-                let wall_size = Vec3::new(2.0, 2.0, 2.0) * wall_scale.x;
-                let wall_pos = wall_transform.translation();
+//                 if let Some(collision) = collision {
+//                     match collision {
+//                         bevy::sprite::collide_aabb::Collision::Left => {
+//                             println!("Main Collision Letf");
+//                             player_transform.translation.x =
+//                                 wall_pos.x - (wall_size.x + player.size.x) * 0.5;
+//                         }
+//                         bevy::sprite::collide_aabb::Collision::Right => {
+//                             println!("Main Collision Right");
+//                             player_transform.translation.x =
+//                                 wall_pos.x + (wall_size.x + player.size.x) * 0.5;
+//                             // player_transform.translation.x = transform.translation.x + (collider_size.x + player_size.x) * 0.5;
+//                         }
+//                         bevy::sprite::collide_aabb::Collision::Top => {
+//                             println!("Main Collision Top");
+//                             player_transform.translation.z =
+//                                 wall_pos.z - (wall_size.z + player.size.y) * 0.5;
+//                         }
+//                         bevy::sprite::collide_aabb::Collision::Bottom => {
+//                             println!("Main Collision Bottom");
+//                             player_transform.translation.z =
+//                                 wall_pos.z + (wall_size.z + player.size.y) * 0.5;
+//                         }
+//                         bevy::sprite::collide_aabb::Collision::Inside => {
+//                             println!("Main Collision Inside");
+//                             // Gérez le cas où le joueur est à l'intérieur du mur
+//                         }
+//                     }
+//                     println!(
+//                         "Player pos: {:?}, Wall pos: {:?}",
+//                         player_transform.translation,
+//                         wall_pos
+//                     );
+//                 }
+//             }
+//         }
+//     }
+// }
 
-                let collision = collide(
-                    player_transform.translation,
-                    Vec2::new(player.size.x, player.size.y),
-                    wall_pos,
-                    Vec2::new(wall_size.x, wall_size.z),
-                );
-
-                if let Some(collision) = collision {
-                    match collision {
-                        bevy::sprite::collide_aabb::Collision::Left => {
-                            println!("Main Collision Letf");
-                            player_transform.translation.x = wall_pos.x - (wall_size.x + player.size.x) * 0.5;
-                        }
-                        bevy::sprite::collide_aabb::Collision::Right => {
-                            println!("Main Collision Right");
-                            player_transform.translation.x = wall_pos.x + (wall_size.x + player.size.x) * 0.5;
-                            // player_transform.translation.x = transform.translation.x + (collider_size.x + player_size.x) * 0.5;
-                        }
-                        bevy::sprite::collide_aabb::Collision::Top => {
-                            println!("Main Collision Top");
-                            player_transform.translation.z = wall_pos.z - (wall_size.z + player.size.y) * 0.5;
-                        }
-                        bevy::sprite::collide_aabb::Collision::Bottom => {
-                            println!("Main Collision Bottom");
-                            player_transform.translation.z = wall_pos.z + (wall_size.z + player.size.y) * 0.5;
-                        }
-                        bevy::sprite::collide_aabb::Collision::Inside => {
-                            println!("Main Collision Inside");
-                            // Gérez le cas où le joueur est à l'intérieur du mur
-                        }
-                    }
-                    println!("Player pos: {:?}, Wall pos: {:?}", player_transform.translation, wall_pos);
-                }
-            }
-        }
-    }
-}
-
-fn update_minimap(
-    player_query: Query<&Transform, With<Player>>,
-    mut minimap_query: Query<&mut Transform, With<MinimapPlayer>>,
-) {
-    if let Ok(player_transform) = player_query.get_single() {
-        if let Ok(mut minimap_transform) = minimap_query.get_single_mut() {
-            // Met à jour la position du joueur sur la minimap
-            minimap_transform.translation = Vec3::new(
-                200.0 + player_transform.translation.x * 10.0, // Ajuste l'échelle pour la minimap
-                200.0 + player_transform.translation.z * 10.0,
-                0.0,
-            );
-        }
-    }
-}
+// fn update_minimap(
+//     player_query: Query<&Transform, With<Player>>,
+//     mut minimap_query: Query<&mut Transform, With<MinimapPlayer>>
+// ) {
+//     if let Ok(player_transform) = player_query.get_single() {
+//         if let Ok(mut minimap_transform) = minimap_query.get_single_mut() {
+//             // Met à jour la position du joueur sur la minimap
+//             minimap_transform.translation = Vec3::new(
+//                 200.0 + player_transform.translation.x * 10.0, // Ajuste l'échelle pour la minimap
+//                 200.0 + player_transform.translation.z * 10.0,
+//                 0.0
+//             );
+//         }
+//     }
+// }
 
 // fn debug_draw_system(
 //     mut gizmos: Gizmos,
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,1 +1,71 @@
-fn main() {}
+use std::net::UdpSocket;
+use std::time::SystemTime;
+
+use bevy_renet::transport::NetcodeServerPlugin;
+use bevy_renet::{ renet::*, RenetServerPlugin };
+use bevy::prelude::{ App, EventReader, ResMut };
+use bevy::app::*;
+use transport::{ NetcodeServerTransport, ServerConfig };
+fn main() {
+    let mut app = App::new();
+    app.add_plugins(RenetServerPlugin);
+
+    let server = RenetServer::new(ConnectionConfig::default());
+    app.insert_resource(server);
+
+    // Transport layer setup
+    app.add_plugins(NetcodeServerPlugin);
+    let server_addr = "127.0.0.1:5000".parse().unwrap();
+    let socket = UdpSocket::bind(server_addr).unwrap();
+    let server_config = ServerConfig {
+        current_time: SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap(),
+        max_clients: 64,
+        protocol_id: 0,
+        public_addresses: vec![server_addr],
+        authentication: transport::ServerAuthentication::Unsecure,
+    };
+    let transport = NetcodeServerTransport::new(server_config, socket).unwrap();
+    app.insert_resource(transport);
+
+    app.add_systems(Update, send_message_system);
+    app.add_systems(Update, receive_message_system);
+    app.add_systems(Update, handle_events_system);
+
+    // Systems
+
+    fn send_message_system(mut server: ResMut<RenetServer>) {
+        let channel_id = 0;
+        // Send a text message for all clients
+        // The enum DefaultChannel describe the channels used by the default configuration
+        server.broadcast_message(DefaultChannel::ReliableOrdered, "server message");
+    }
+
+    fn receive_message_system(mut server: ResMut<RenetServer>) {
+        // Receive message from all clients
+        for client_id in server.clients_id() {
+            while
+                let Some(message) = server.receive_message(
+                    client_id,
+                    DefaultChannel::ReliableOrdered
+                )
+            {
+                // Handle received message
+                println!("received message => {:?}", message);
+            }
+        }
+    }
+
+    fn handle_events_system(mut server_events: EventReader<ServerEvent>) {
+        for event in server_events.read() {
+            match event {
+                ServerEvent::ClientConnected { client_id } => {
+                    println!("Client {client_id} connected");
+                }
+                ServerEvent::ClientDisconnected { client_id, reason } => {
+                    println!("Client {client_id} disconnected: {reason}");
+                }
+            }
+        }
+    }
+    app.run()
+}
