eb1e6ba307d6326993100d6d5235d622f2ff3b7b
mass <massaw6@gmail.com>
2024-08-19 00:38:46 +0000

    new: constant module (stores static shared by both client and server)

--- store/src/lib.rs
+++ store/src/lib.rs
@@ -1,12 +1,12 @@
 mod event;
 mod player;
 mod game_state;
+mod constant;
 
+pub use constant::*;
 pub use game_state::*;
 pub use player::*;
 pub use event::*;
-// use serde::{ Deserialize, Serialize };
-// use std::collections::HashMap;
 
 
 
