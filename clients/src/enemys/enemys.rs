9c51269d0ee5ff8ac29bf72fe5725c7d3bb43e33
nifaye <nicolasfaye31@gmail.com>
2024-09-02 17:49:20 +0000

    debug: simuler les enemies avec des figure geo

--- clients/src/enemys/enemys.rs
+++ clients/src/enemys/enemys.rs
@@ -25,103 +25,72 @@ impl Enemy {
     }
 }
 
-
-use bevy::prelude::*;
-use bevy_rapier3d::prelude::*;
-
 pub fn create_enemys(
     commands: &mut Commands,
     list_player: &ListPlayer,
     meshes: &mut ResMut<Assets<Mesh>>,
-    materials: &mut ResMut<Assets<StandardMaterial>>
+    materials: &mut ResMut<Assets<StandardMaterial>>,
 ) {
     println!("------------Enemys-------{:?}", list_player.list);
     
     for (&id, player) in list_player.list.iter() {
-        let soldier_entity = commands.spawn(SpatialBundle::default())
-            .insert(Enemy::new(id, format!("Enemy_{}", id), player.position.clone()))
-            .insert(RigidBody::KinematicPositionBased)
-            .insert(Collider::capsule(0.5, 0.2))
-            .insert(Velocity::default())
-            .insert(Name::new(format!("Enemy_{}", id)))
-            .id();
-
-        // Corps (torse)
-        commands.spawn(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Capsule { 
-                radius: 0.2, 
-                rings: 3, 
-                depth: 0.5, 
-                latitudes: 8, 
-                longitudes: 16, 
-                uv_profile: shape::CapsuleUvProfile::Aspect 
-            })),
-            material: materials.add(Color::DARK_GRAY.into()),
-            transform: Transform::from_xyz(0.0, 0.5, 0.0),
-            ..default()
-        }).set_parent(soldier_entity);
-
-        // Tête
-        commands.spawn(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::UVSphere { 
-                radius: 0.15, 
-                sectors: 16, 
-                stacks: 16 
-            })),
-            material: materials.add(Color::BEIGE.into()),
-            transform: Transform::from_xyz(0.0, 0.9, 0.0),
-            ..default()
-        }).set_parent(soldier_entity);
-
-        // Bras gauche
-        commands.spawn(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Capsule { 
-                radius: 0.05, 
-                rings: 3, 
-                depth: 0.4, 
-                latitudes: 8, 
-                longitudes: 16, 
-                uv_profile: shape::CapsuleUvProfile::Aspect 
-            })),
-            material: materials.add(Color::DARK_GRAY.into()),
-            transform: Transform::from_xyz(-0.25, 0.5, 0.0)
-                .with_rotation(Quat::from_rotation_x(0.5)),
-            ..default()
-        }).set_parent(soldier_entity);
-
-        // Bras droit (tenant l'arme)
-        commands.spawn(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Capsule { 
-                radius: 0.05, 
-                rings: 3, 
-                depth: 0.4, 
-                latitudes: 8, 
-                longitudes: 16, 
-                uv_profile: shape::CapsuleUvProfile::Aspect 
-            })),
-            material: materials.add(Color::DARK_GRAY.into()),
-            transform: Transform::from_xyz(0.25, 0.5, 0.0)
-                .with_rotation(Quat::from_rotation_z(-0.5)),
-            ..default()
-        }).set_parent(soldier_entity);
-
-        // AK-47 (représentation simplifiée)
-        commands.spawn(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Box::new(0.5, 0.1, 0.05))),
-            material: materials.add(Color::BLACK.into()),
-            transform: Transform::from_xyz(0.4, 0.4, 0.2)
-                .with_rotation(Quat::from_rotation_y(0.2)),
-            ..default()
-        }).set_parent(soldier_entity);
-
-        // Positionnement global du soldat
-        commands.entity(soldier_entity).insert(Transform::from_xyz(
+        let enemy = Enemy::new(id, format!("Enemy_{}", id), player.position.clone());
+        let transform = Transform::from_xyz(
             player.position.x,
             player.position.y,
             player.position.z
-        ));
+        );
 
-        println!("Spawned enemy with ID: {:?}", soldier_entity);
+        // Corps
+        let body = commands.spawn(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Capsule {
+                radius: 0.25,
+                rings: 0,
+                depth: 1.0,
+                latitudes: 16,
+                longitudes: 32,
+                uv_profile: shape::CapsuleUvProfile::Uniform
+            })),
+            material: materials.add(Color::GRAY.into()),
+            transform: transform.with_scale(Vec3::new(0.5, 0.5, 0.5)),
+            ..default()
+        }).id();
+
+        // Tête
+        let head = commands.spawn(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::UVSphere {
+                radius: 0.3,
+                sectors: 32,
+                stacks: 16,
+            })),
+            material: materials.add(Color::ANTIQUE_WHITE.into()),
+            transform: Transform::from_xyz(0.0, 0.8, 0.0),
+            ..default()
+        }).id();
+
+        // Arme (AK-47 simplifié)
+        let weapon = commands.spawn(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Box::new(0.1, 0.3, 1.0))),
+            material: materials.add(Color::BLACK.into()),
+            transform: Transform::from_xyz(0.3, 0.0, 0.5).with_rotation(Quat::from_rotation_x(-0.2)),
+            ..default()
+        }).id();
+
+        // Assembler le soldat
+        let soldier = commands.spawn((
+            enemy,
+            SpatialBundle::from_transform(transform),
+            RigidBody::KinematicPositionBased,
+            Collider::capsule(Vec3::new(0.0, -0.5, 0.0), Vec3::new(0.0, 0.5, 0.0), 0.25),
+            Velocity::default(),
+        ))
+        .add_child(body)
+        .add_child(head)
+        .add_child(weapon)
+        .insert(Name::new(format!("Enemy_{}", id)))
+        .id();
+
+        println!("Spawned enemy with ID: {:?}", soldier);
     }
 }
 
@@ -129,7 +98,6 @@ pub fn update_enemys_position(
     mut query: Query<(&mut Transform, &mut Enemy)>,
     mut commands: Commands,
     list_player: Res<ListPlayer>,
-    asset_server: Res<AssetServer>,
     game_state: Res<GameState>,
     mut ennemy_created: ResMut<EnnemyCreated>,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -137,10 +105,10 @@ pub fn update_enemys_position(
 ) {
     if game_state.has_started && ennemy_created.val {
         println!("❌❌❌❌");
-        create_enemys(&mut commands, &list_player, &asset_server);
+        create_enemys(&mut commands, &list_player, &mut meshes, &mut materials);
         ennemy_created.val = false;
     }
-
+    
     for (mut transform, mut enemy) in query.iter_mut() {
         if let Some(player) = list_player.list.get(&enemy.id) {
             enemy.position = player.position.clone();
