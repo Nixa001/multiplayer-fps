6d581ff18a52221859540378b824d502ae68aba2
nifaye <nicolasfaye31@gmail.com>
2024-09-03 11:28:59 +0000

    debug: 6.0

--- clients/src/player/player.rs
+++ clients/src/player/player.rs
@@ -198,7 +198,7 @@ pub fn setup_player_and_camera(
     // Spawn the camera and attach it to the weapon
     commands
         .spawn((Camera3dBundle {
-            transform: Transform::from_xyz(-0.25, 0.8, 0.0), // Adjust camera position relative to a weapon
+            transform: Transform::from_xyz(-0.25, 0.7, 0.0), // Adjust camera position relative to a weapon
             ..default()
         },))
         .set_parent(player_entity);
