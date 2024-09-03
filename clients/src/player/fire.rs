a32a2ae1a56a58481056739810660c0eee8985de
nifaye <nicolasfaye31@gmail.com>
2024-09-03 12:28:06 +0000

    debug 6.9

--- clients/src/player/fire.rs
+++ clients/src/player/fire.rs
@@ -5,12 +5,12 @@ use bevy::prelude::*;
 use bevy::window::Window;
 use bevy_rapier3d::prelude::*;
 
-// #[derive(Component)]
+#[derive(Component)]
 #[allow(dead_code)]
-// pub struct Projectile {
-//     pub speed: f32,
-//     pub lifetime: Timer,
-// }
+pub struct Projectile {
+    pub speed: f32,
+    pub lifetime: Timer,
+}
 
 #[derive(Bundle)]
 pub struct ProjectileBundle {
@@ -20,14 +20,8 @@ pub struct ProjectileBundle {
     collider: Collider,
     velocity: Velocity,
 }
-#[derive(Component)]
-pub struct Projectile {
-    pub speed: f32,
-    pub lifetime: Timer,
-    pub initial_velocity: Vec3,
-    pub gravity: f32,
-}
 
+#[allow(dead_code)]
 pub fn fire_projectile(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -43,25 +37,25 @@ pub fn fire_projectile(
                 let window = windows.single();
                 let center = Vec2::new(window.width() / 2.0, window.height() / 2.0 - 20.);
 
-                if let Some(ray) = ray_from_screenspace(&camera, camera_transform, center) {
-                    let spawn_point = player_transform.translation + player_transform.forward() * 0.60;
+                if let Some(ray) = ray_from_screenspace(&camera, camera_transform, center)
+                {
+                    let spawn_point =
+                        player_transform.translation + player_transform.forward() * 0.60;
                     let projectile_direction = ray.direction;
 
-                    let initial_speed = 50.0; // Ajustez cette valeur selon vos besoins
-                    let initial_velocity = projectile_direction * initial_speed;
-
-                    commands.spawn((
-                        Projectile {
-                            speed: initial_speed,
+                    commands.spawn(ProjectileBundle {
+                        projectile: Projectile {
+                            speed: 100.0,
                             lifetime: Timer::from_seconds(5.0, TimerMode::Once),
-                            initial_velocity,
-                            gravity: 9.81, // Ajustez cette valeur selon vos besoins
                         },
-                        PbrBundle {
-                            mesh: meshes.add(Mesh::try_from(shape::Icosphere {
-                                radius: 0.01,
-                                subdivisions: 1,
-                            }).unwrap()),
+                        pbr_bundle: PbrBundle {
+                            mesh: meshes.add(
+                                Mesh::try_from(shape::Icosphere {
+                                    radius: 0.01,
+                                    subdivisions: 1,
+                                })
+                                .unwrap(),
+                            ),
                             material: materials.add(StandardMaterial {
                                 base_color: Color::ORANGE_RED,
                                 emissive: Color::rgba_linear(1.0, 0.0, 0.0, 1.0),
@@ -70,22 +64,24 @@ pub fn fire_projectile(
                             transform: Transform::from_translation(spawn_point),
                             ..default()
                         },
-                        RigidBody::Dynamic,
-                        Collider::ball(0.03),
-                        Velocity::linear(initial_velocity),
-                    ));
+                        rigid_body: RigidBody::KinematicVelocityBased,
+                        collider: Collider::ball(0.03),
+                        velocity: Velocity::linear(projectile_direction * 50.0),
+                    });
                 }
             }
         }
     }
 }
 
+#[allow(dead_code)]
 pub fn update_projectiles(
     mut commands: Commands,
-    mut projectile_query: Query<(Entity, &mut Projectile, &mut Transform, &mut Velocity)>,
+    mut projectile_query: Query<(Entity, &mut Projectile, &Velocity)>,
     time: Res<Time>,
+    rapier_context: Res<RapierContext>,
 ) {
-    for (entity, mut projectile, mut transform, mut velocity) in projectile_query.iter_mut() {
+    for (entity, mut projectile, velocity) in projectile_query.iter_mut() {
         projectile.lifetime.tick(time.delta());
 
         if projectile.lifetime.finished() {
@@ -93,15 +89,14 @@ pub fn update_projectiles(
             continue;
         }
 
-        // Mise à jour de la vélocité avec la gravité
-        velocity.linvel += Vec3::new(0.0, -projectile.gravity, 0.0) * time.delta_seconds();
+        let ray_origin = velocity.linvel.normalize() * 0.05;
+        let ray_direction = velocity.linvel.normalize();
 
-        // Mise à jour de la position
-        transform.translation += velocity.linvel * time.delta_seconds();
-
-        // Rotation du projectile pour qu'il pointe dans la direction du mouvement
-        if velocity.linvel.length_squared() > 0.01 {
-            transform.look_to(velocity.linvel, Vec3::Y);
+        if rapier_context
+            .cast_ray(ray_origin, ray_direction, 0.1, true, QueryFilter::default())
+            .is_some()
+        {
+            commands.entity(entity).despawn();
         }
     }
 }
@@ -149,6 +144,7 @@ pub fn handle_projectile_collisions(
                 //     println!("Ennemi éliminé !");
                 // }
 
+                // Sortir de la boucle interne car le projectile a déjà touché un ennemi
                 break;
             }
         }
